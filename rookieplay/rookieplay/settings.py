9b9f08037d5636aeb6667dbbe3ec0c7a627953f3
Ubuntu <ubuntu@ip-172-31-94-98.ec2.internal>
2021-06-16 20:02:41 +0000

    configuring server

--- rookieplay/rookieplay/settings.py
+++ rookieplay/rookieplay/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'nh_n3_2tgd0=f)1s4y23v8(k^o^h)@7^gdlhbb%_jv1zkmz^%a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES = [STATIC_ROOT]
